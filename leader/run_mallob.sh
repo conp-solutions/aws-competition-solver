f3920b8c728a5c494324fe29584dd41d494acc1c
Norbert Manthey <nmanthey@conp-solutions.com>
2022-06-07 22:09:28 +0200

    mergesat: use absolute path for tool
    Signed-off-by: Norbert Manthey <nmanthey@conp-solutions.com>

--- leader/run_mallob.sh
+++ leader/run_mallob.sh
@@ -2,7 +2,7 @@
 
 # MergeSat, ignore host file, just consume input file
 
-# Rn on formula (parameter $2), with half the available cores
-mergesat "$2" -no-model -cores=-2
+# Run on formula (parameter $2), with half the available cores
+/mergesat "$2" -no-model -cores=-2
 
 exit 0
